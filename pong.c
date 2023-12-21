aed8bd552c39e5bbd6b600f2161de2057be12c2e
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:38:22 +0100

    [+] Add a fabs to the acos

--- pong.c
+++ pong.c
@@ -16,8 +16,8 @@ static int calculate_angle(char **argv, my_vector_t *v)
     printf("The incidence angle is:\n");
     if (pow(v->x * v->x + v->y * v->y + v->z * v->z, 1. / 2.) == 0.)
         return printf("0.00 degrees");
-    printf("%.2lf degrees\n", 180. / PI * acos(v->z /
-        pow(v->x * v->x + v->y * v->y + v->z * v->z, 1. / 2.)) - 90.);
+    printf("%.2lf degrees\n", 180. / PI * fabs(acos(v->z /
+        pow(v->x * v->x + v->y * v->y + v->z * v->z, 1. / 2.)) - 90.));
     return 0;
 }
 
